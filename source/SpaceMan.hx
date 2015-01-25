ffb81440229b18bd8731fe081ee720456cccd7c0
Shannon Phillips <sphillips@nucloudglobal.com>
2015-01-24 18:28:09 -0600

    Resolved merge conflicts

--- source/SpaceMan.hx
+++ source/SpaceMan.hx
@@ -1,5 +1,11 @@
 package ;
 import flixel.addons.nape.FlxNapeSprite;
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.input.gamepad.FlxGamepad;
+import flixel.input.gamepad.FlxGamepadManager;
+import flixel.input.gamepad.XboxButtonID;
+
 using Std;
 /**
  * ...
@@ -7,21 +13,51 @@ using Std;
  */
 class SpaceMan extends FlxNapeSprite
 {
-	public var myWidth = 100.0;
-	public var myHeight = 310.0;
+	// User properties
+	public static inline var myWidth = 100.0;
+	public static inline var myHeight = 310.0;
+	public static inline var jumpHeight = 310;
+	public static inline var myVelocity = 500;
+	// User status
+	public var weapon;
+	public var health;
+	// User gamepad
+	private var _gamePad:FlxGamepad;
+	public var jump; // Controller:A Keyboard:S
+	public var use; // Controller:X Keyboard:A 
+	public var fire; // Controller:B Keyboard:D
+	public var move_left; // Direction controls are keyboard arrow keys or Controller DPAD
+	public var move_right;
+	public var move_up;
+	public var move_down;
+	
 	
 	public function new(X, Y) 
 	{
 		super();
-		makeGraphic(myWidth.int(), myHeight.int(), 0x0);
+		jump = false;
+		use = false;
+		fire = false;
+		move_up = false;
+		move_down = false;
+		move_right = false;
+		move_left = false;
+		
+		weapon = null;
+		
+		makeGraphic(myWidth.int(), myHeight.int(), 0xFFFFFFFF);
 		createRectangularBody();
 		loadGraphic('assets/spaceman.png');
 		setBodyMaterial(.5, .5, .5, 2);
 		body.position.y = Y;
 		body.position.x = X;
 		body.allowRotation = false;
-		origin.x = 118.0;
-		origin.y = 35.0;
+	}
+	
+	override public function update()
+	{
+		super.update();
+		updateWeapon();
 	}
 	
 }
--- source/states/Level00.hx
+++ source/states/Level00.hx
@@ -17,24 +17,35 @@ using Std;
  */
 class Level00 extends FlxNapeState
 {
+<<<<<<< Updated upstream
 	static public inline var FNAME  = "assets/level1.png";
 	static public inline var CELL_W = 100.0;
 	static public inline var CELL_H = 100.0;
 
-	var loader : LevelLoader;
+	var loader : LevelLoader;	
 	var fps:FPS;
-	
+	public var players:Array;
+
 	override public function create() 
 	{
 		super.create();
 		add(new FlxSprite(0, 0, "assets/Environment_1.png"));
 		loader = new LevelLoader(this, FNAME, CELL_W, CELL_H);
 		loader.load();
+		FlxNapeState.space.gravity.setxy(0, 500);
 	}
 	
 	public function createFloor(X:Float, Y:Float, W:Float, H:Float) {
 		var floor = new FlxNapeSprite(X, Y);
 		floor.makeGraphic(W.int(), H.int(), 0xFFFFFFFF);
+		floor.createRectangularBody(0, 0, BodyType.STATIC);
+		floor.setBodyMaterial(.5, .5, .5, 2);
+		return floor;
+	}
+	
+	public function createWall(X:Float, Y:Float, W:Float, H:Float) {
+		var floor = new FlxNapeSprite(X, Y);
+		floor.makeGraphic(W.int(), H.int(), 0xFFFFFFAA);
 		floor.createRectangularBody(0, 0, BodyType.STATIC);
 		floor.setBodyMaterial(.5, .5, .5, 2);
 		return floor;
